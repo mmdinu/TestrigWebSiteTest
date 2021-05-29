4e29e08b935166ff9a909f97294bf1262fd404f6
mmdinu <75750050+mmdinu@users.noreply.github.com>
2021-05-29 09:49:23 +0300

    Final! modificat selector Blog!

--- src/test/java/Tests/BlogPage.java
+++ src/test/java/Tests/BlogPage.java
@@ -13,21 +13,21 @@ public class BlogPage extends BaseTest {
 
         CloseMessenger();
         SwitchToDefaultFrame();
-        WebElement PopularWeb = driver.findElement(By.xpath("//*[@id='blogs']/div/div/div[1]/div/div/a"));
+        WebElement PopularWeb = driver.findElement(By.xpath("//a[normalize-space()='Blog']"));
         PopularWeb.click();
 
         CloseMessenger();
         SwitchToDefaultFrame();
-        WebElement Top3ApiWeb = driver.findElement(By.xpath("//*[@id='blogs']/div/div[1]/div[1]/h6/a"));
+        WebElement Top3ApiWeb = driver.findElement(By.xpath("//a[normalize-space()='What are the Top 3 API Testing Tools of 2021?']"));
         Top3ApiWeb.click();
         driver.navigate().back();
 
-//        CloseMessenger();
-//        SwitchToDefaultFrame();
-//        WebElement TopQAAutoWeb = driver.findElement(By.xpath("//*[@id='blogs']/div/div[1]/div[2]/h6/a"));
-//        TopQAAutoWeb.click();
-//        driver.navigate().back();
-//
+        CloseMessenger();
+        SwitchToDefaultFrame();
+        WebElement TopQAAutoWeb = driver.findElement(By.xpath("//a[contains(text(),'Top QA Automation Testing Service Providers in USA')]"));
+        TopQAAutoWeb.click();
+        driver.navigate().back();
+
 //        CloseMessenger();
 //        SwitchToDefaultFrame();
 //        WebElement TopMobileAppWeb = driver.findElement(By.xpath("//*[@id='blogs']/div/div[1]/div[9]/h6/a"));
@@ -38,11 +38,7 @@ public class BlogPage extends BaseTest {
 //        SwitchToDefaultFrame();
 //        WebElement BlogPg4Web = driver.findElement(By.xpath("//*[@id='blogs']/div/div[2]/ul/li[4]/a"));
 //        BlogPg4Web.click();
-//
-//        CloseMessenger();
-//        SwitchToDefaultFrame();
-//        WebElement GuideTPTWeb = driver.findElement(By.xpath("//*[@id='blogs']/div/div[1]/div[1]/h6/a"));
-//        GuideTPTWeb.click();
+
 
 
         driver.close();
