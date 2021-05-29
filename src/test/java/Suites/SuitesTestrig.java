3a574c1f4e9550b52b89feb59c0191af8642abb1
mmdinu <75750050+mmdinu@users.noreply.github.com>
2021-05-29 09:11:09 +0300

    Final cu mai multe comentari, altfel ruleaza in cca 12 minute!

--- src/test/java/Suites/SuitesTestrig.java
+++ src/test/java/Suites/SuitesTestrig.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({HomePage.class, ServicePage.class, RequirementForm.class, DomainsPage.class, HireQAPage.class, BlogPage.class, WebAutomationTest.class})
+@Suite.SuiteClasses({HomePage.class, ServicePage.class, DomainsPage.class, HireQAPage.class, RequirementForm.class, BlogPage.class, WebAutomationTest.class})
 
 public class SuitesTestrig {
 }
